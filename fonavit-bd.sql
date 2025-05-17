00294a3bb5a90323cd315692acda8d7908354f97
Daniel <dfquack08@gmail.com>
2025-05-16 20:08:25 -0600

    Tipos de datos actualizados

--- fonavit-bd.sql
+++ fonavit-bd.sql
@@ -3,32 +3,32 @@ USE fonavit_bd;
 
 -- Creación de tablas
 CREATE TABLE empleado (
-  carnet varchar PRIMARY KEY,
-  dui varchar,
-  nombre varchar,
+  carnet char(8) PRIMARY KEY,
+  dui char(10),
+  nombre varchar(50),
   fecha_nac date,
   edad tinyint,
-  direccion varchar,
-  puesto varchar,
+  direccion varchar(100),
+  puesto varchar(20),
   sueldo smallmoney,
-  carnet_jefe varchar
+  carnet_jefe char(8)
 );
 
 CREATE TABLE cliente (
   num_cuenta int PRIMARY KEY IDENTITY(1,1),
-  dui varchar,
-  nombre varchar,
+  dui char(10),
+  nombre varchar(50),
   fecha_nac date,
   edad tinyint,
-  direccion varchar,
-  estado_fam varchar
+  direccion varchar(100),
+  estado_fam varchar(10)
 );
 
 CREATE TABLE transaccion (
-  id varchar PRIMARY KEY,
-  carnet varchar FOREIGN KEY REFERENCES empleado(carnet),
+  id char(6) PRIMARY KEY,
+  carnet char(8) FOREIGN KEY REFERENCES empleado(carnet),
   num_cuenta int FOREIGN KEY REFERENCES cliente(num_cuenta),
-  dui varchar,
+  dui char(10),
   monto smallmoney,
   fecha date
 );
@@ -36,7 +36,7 @@ CREATE TABLE transaccion (
 CREATE TABLE prestamo (
   id varchar PRIMARY KEY,
   num_cuenta int FOREIGN KEY REFERENCES cliente(num_cuenta),
-  dui varchar,
+  dui char(10),
   monto smallmoney,
   interes tinyint,
   fecha date,
