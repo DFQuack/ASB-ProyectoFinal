8334d244f5ccd4599c45ff47b950d89e68bc169d
Brandon876723 <Brandoncenteno534@gmail.com>
2025-05-18 14:54:16 -0600

    Update fonavit-bd.sql

--- fonavit-bd.sql
+++ fonavit-bd.sql
@@ -42,3 +42,28 @@ CREATE TABLE prestamo (
   fecha date,
   plazo tinyint
 );
+
+CREATE LOGIN administrador WITH PASSWORD = '1234';
+CREATE LOGIN sistema WITH PASSWORD = '4321';
+CREATE LOGIN webservice WITH PASSWORD = '0123';
+
+CREATE USER administrador FOR LOGIN administrador;
+CREATE USER sistema FOR LOGIN sistema;
+CREATE USER webservice FOR LOGIN webservice;
+
+GRANT SELECT, INSERT, DELETE, UPDATE TO administrador;
+GRANT ALTER ANY LOGIN TO administrador;
+GRANT ALTER ANY USER TO administrador;
+GRANT CONTROL SERVER TO administrador;
+
+GRANT SELECT, INSERT ON empleado TO sistema;
+GRANT SELECT, INSERT ON cliente TO sistema;
+GRANT SELECT, INSERT ON transaccion TO sistema;
+GRANT SELECT, INSERT ON prestamo TO sistema;
+
+-- Creado en bloque aparte
+CREATE VIEW vistaTransacciones AS 
+SELECT id, carnet, num_cuenta, dui, monto, fecha
+FROM transaccion;
+
+GRANT SELECT ON vistaTransacciones TO webservice;
